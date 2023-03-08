afb5d9ccd7b7950322ad4e11cea9de754a4157c7
halannale <halanna.le@ucalgary.ca>
2023-03-07 21:19:36 -0700

    menu button works

--- src/Layout.js
+++ src/Layout.js
@@ -3,10 +3,20 @@ import "./App.css";
 import Side from "./side";
 
 function Layout(props) {
+    const menu = () => {
+        const sideDisplay = document.getElementsByClassName("app-side")[0];
+        if (sideDisplay.style.display === "none") {
+          sideDisplay.style.display = "block";
+        }
+        else {
+          sideDisplay.style.display = "none";
+        }
+    }
+
     return (
         <div>
         <div className="top">
-        <button onClick="Menu()">&#9776;</button>
+        <button onClick={menu}>&#9776;</button>
         <div className="top-header">
             <h1>Lotion</h1>
             <p>Like Notion, but worse.</p>
